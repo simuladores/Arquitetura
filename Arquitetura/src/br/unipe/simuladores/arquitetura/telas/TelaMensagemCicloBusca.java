b6dacfb319a4f8ca882a47a0480839e684a3a8bb
Simuladores Unipe <desenvolvimentodesimuladores@googlegroups.com>
2012-01-22 23:10:58 -0300

    commit

--- Arquitetura/src/br/unipe/simuladores/arquitetura/telas/TelaMensagemCicloBusca.java
+++ Arquitetura/src/br/unipe/simuladores/arquitetura/telas/TelaMensagemCicloBusca.java
@@ -11,21 +11,24 @@ public class TelaMensagemCicloBusca extends TelaMensagemSimulacao{
 	
 	private static final String ATUALIZAR_PCTXT = "Será dado início ao ciclo de busca, " +
 			"para buscar a próxima instrução na memória.\n Primeiro, o valor de PC é " +
-			"atualizado com o endereço da instrução que será\n executada em seguida";
+			"atualizado com o endereço da instrução que será\n executada em seguida.";
 	private static final String MOVER_MARTXT = "O valor de PC é então transferido para" +
 			" MAR, que contém os endereços\n que são tranferidos do barramento para a " +
-			"memória e que chegam\n da memória através do barramento" ;
+			"memória e que chegam\n da memória através do barramento." ;
 	private static final String COPIAR_READ_BARRAMENTOTXT = "A unidade de controle é " +
 			"responsável por coordenar as atividades da UCP.\n Neste caso, ela envia o " +
 			"comando \"READ\" para o barramento, significando\n que a UCP está requisi" +
-			"tando uma leitura de dados da memória";
+			"tando uma leitura de dados da memória.";
 	private static final String COPIAR_VALOR_MAR_BARRAMENTOTXT = "Assim como o comando " +
 			"\"READ\", o valor de MAR, que contém o endereço\n da próxima instrução, " +
-			"também é transferido para o barramento";
+			"também é transferido para o barramento.";
 	private static final String MOVER_DADOS_BARRAMENTO_MEMORIATXT = "Esses dois valores " +
 			"serão transferidos para a memória, pelo barramento.\n O valor de MAR " +
-			"no barramento de endereços e o comando \"READ\"\n no barramento de instruções";
-	private static final String TRANSFERIR_INSTRUCAOTXT = "A instrução é então lida da memória";
+			"no barramento de endereços e o comando \"READ\"\n no barramento de instruções.";
+	private static final String TRANSFERIR_INSTRUCAOTXT = "A instrução requisitada é " +
+			"então transferida para a UCP pelo barramento de dados.\n " +
+			"Inicialmente, o registrador que conterá a instrução será o MBR, já que " +
+			"é ele\n que armazena os dados ou instruções logo que chegam da memória.";
 	
 	public TelaMensagemCicloBusca(EstadoCiclo estado) {
 		super("Mensagem", Color.rgb(245, 245, 245));
